917b9b0996fb969f6a26f655d8448bc1d79a003b
jaimein <jaimeiserte@hotmail.com>
2020-01-17 19:37:00 +0100

    formateamos archivos

--- listanoadmin.php
+++ listanoadmin.php
@@ -1,42 +1,42 @@
 <div class="login"><a href="./admin/index.php">Login</a></div>
 <div>
-<h1>Listado de Fiestas</h1>
-<?php
+    <h1>Listado de Fiestas</h1>
+    <?php
 
-include 'conexion.php';
+    include 'conexion.php';
 
-$query = "SELECT *  "
+    $query = "SELECT *  "
         . "FROM buena ";
-if ($stmt = $conexion->prepare($query)) {
-    if (!$stmt->execute()) {
-        die('Error de ejecución de la consulta. ' . $conexion->error);
-    }
-    //echo $query;
-    // recoger los datos
-    $stmt->bind_result($fecha, $grupo, $poblacion, $tipo);
-    //cabecera de los datos mostrados
-    echo "<table>"; //start table
-    //creating our table heading
-    echo "<tr>";
-    echo "<th>Fecha</th>";
-    echo "<th>Grupo</th>";
-    echo "<th>Poblacion</th>";
-    echo "<th>Tipo</th>";
-    echo "</tr>";
-    //recorrido por el resultado de la consulta
-    while ($stmt->fetch()) {
+    if ($stmt = $conexion->prepare($query)) {
+        if (!$stmt->execute()) {
+            die('Error de ejecución de la consulta. ' . $conexion->error);
+        }
+        //echo $query;
+        // recoger los datos
+        $stmt->bind_result($fecha, $grupo, $poblacion, $tipo);
+        //cabecera de los datos mostrados
+        echo "<table>"; //start table
+        //creating our table heading
         echo "<tr>";
-        echo '<td>'.$fecha . '</td>';
-        echo '<td>' . $grupo . '</td>';
-        echo '<td>' . $poblacion . '</td>';
-        echo '<td>' . $tipo . '</td>';
-        echo "</tr>\n";
+        echo "<th>Fecha</th>";
+        echo "<th>Grupo</th>";
+        echo "<th>Poblacion</th>";
+        echo "<th>Tipo</th>";
+        echo "</tr>";
+        //recorrido por el resultado de la consulta
+        while ($stmt->fetch()) {
+            echo "<tr>";
+            echo '<td>' . $fecha . '</td>';
+            echo '<td>' . $grupo . '</td>';
+            echo '<td>' . $poblacion . '</td>';
+            echo '<td>' . $tipo . '</td>';
+            echo "</tr>\n";
+        }
+        // end table
+        echo "</table>";
+        $stmt->close();
+    } else {
+        die('Imposible preparar la consulta. ' . $conexion->error);
     }
-    // end table
-    echo "</table>";
-    $stmt->close();
-} else {
-    die('Imposible preparar la consulta. ' . $conexion->error);
-}
-?>
+    ?>
 </div>
